5ba72cbfd2edbd618671aaf98d374f6f092c1ac4
Carol Anderia Maria <cm8510823@gmail.com>
2025-02-23 12:22:24 +0530

    commit

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Navbar.css";
-import navlogo from "../../assets/nav-logo.svg";
+import logo from "../../assets/logo.svg";
 import navProfile from "../../assets/nav-profile.svg";
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <img src={navlogo} alt="" className="nav-logo" />
+      <img src={logo} alt="" className="nav-logo" />
       <img src={navProfile} alt="" className="nav-profile" />
     </div>
   );
